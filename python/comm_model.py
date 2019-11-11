bff643a066e036341cad02dc219df2e5104b9cbd
“wenkanw” <wenkangwei917@gmail.com>
2019-11-11 17:07:57 -0500

    update comm

--- python/comm_model.py
+++ python/comm_model.py
@@ -112,7 +112,7 @@ class Xbee():
         """
         buf = {}
         for d in data_ls:
-            buf[int(d[0])] ={'t':d[1], 's':d[2],'p':d[3],'d':d[4]}
+            buf[int(d[0])] ={'t':d["1"], 's':d["2"],'p':d["3"],'d':d["4"]}
 
         s = json.dumps(buf)
         fp = open('r_buf.json','w')
