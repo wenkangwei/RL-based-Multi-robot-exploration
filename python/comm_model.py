1d521b835de17fc29dd0a2d2f977ab6ffaf39b6d
“wenkanw” <wenkangwei917@gmail.com>
2019-11-11 19:18:58 -0500

    update

--- python/comm_model.py
+++ python/comm_model.py
@@ -117,7 +117,7 @@ class Xbee():
         """
         buf = {}
         for d in data_ls:
-            buf[d["0"]] ={'t':d["1"], 's':d["2"],'p':d["3"],'d':d["4"]}
+            buf[d[0]] ={'t':d[1], 's':d[2],'p':d[3],'d':d[4]}
 
         s = json.dumps(buf)
         fp = open('r_buf.json','w')
@@ -206,7 +206,7 @@ def comm_agents():
                 xb.send(xb.data)
                 while xb.Available():
                     xb.read()
-                time.sleep(1)
+                # time.sleep(1)
 
             # receive data from other agents with lower priority
             data = ''
