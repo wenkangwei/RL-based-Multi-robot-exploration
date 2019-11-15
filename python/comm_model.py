ced479ab828c3d9bfaf2973cfc53283c830b58ee
“wenkanw” <wenkangwei917@gmail.com>
2019-11-15 13:42:06 -0500

    update

--- python/comm_model.py
+++ python/comm_model.py
@@ -238,7 +238,7 @@ def comm_agents2():
             init_t = time.time()
             cur_t = init_t
             # time out 8 s
-            timeout = 10
+            timeout = 8
 
             # if it is term to send data, but not ready,  check if other agents already timeout and send request
             # if it is not the term to send, keep read data from other agents
@@ -248,8 +248,6 @@ def comm_agents2():
                 cur_t = time.time()
                 while xb.Available():
                     data += xb.read()
-                if len(data)>10:
-                    break
 
             # if received data
             if len(data)>3:
